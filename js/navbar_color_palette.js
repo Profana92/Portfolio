28c73778ef3404a145c84d5b27a8d383ea201818
Profana92 <87131514+Profana92@users.noreply.github.com>
2022-01-16 22:21:14 +0100

    updated.

--- js/navbar_color_palette.js
+++ js/navbar_color_palette.js
@@ -2,7 +2,12 @@ const dark = document.querySelector(".colorbox--dark");
 const night = document.querySelector(".colorbox--night");
 const light = document.querySelector(".colorbox--light");
 const sidebar = document.querySelector(".sidebar");
+const sidebarText = document.getElementsByClassName("sidebar__center__list__item__text");
+const sidebarTopEl = document.querySelector(".sidebar__top__paragraph");
+const sidebarTopElText = document.querySelector(".sidebar__top__paragraph--small");
+const sidebarTopImg = document.querySelector(".sidebar__top__faceimg");
 
+//const hide = document.querySelector(".colorbox--light");
 dark.addEventListener("click", () => {
   sidebar.className = "sidebar colorbox--dark";
 });
@@ -14,3 +19,18 @@ night.addEventListener("click", () => {
 light.addEventListener("click", () => {
   sidebar.className = "sidebar colorbox--light";
 });
+
+light.addEventListener("click", () => {
+  sidebar.className = "sidebar--minimized";
+  hide();
+});
+
+function hide() {
+  console.log(sidebarText);
+  sidebarTopElText.className = "hidden";
+  sidebarTopImg.className = "hidden";
+  sidebarTopEl.className = "hidden";
+  for (let i = 0; i < sidebarText.length; i + 1) {
+    sidebarText[i].className = "hidden";
+  }
+}
