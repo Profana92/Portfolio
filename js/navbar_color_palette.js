8116b72f8b04d862bea5251c2c679dd0d8035d61
Profana92 <87131514+Profana92@users.noreply.github.com>
2022-01-17 22:13:35 +0100

    update

--- js/navbar_color_palette.js
+++ js/navbar_color_palette.js
@@ -1,36 +1,57 @@
-const dark = document.querySelector(".colorbox--dark");
-const night = document.querySelector(".colorbox--night");
-const light = document.querySelector(".colorbox--light");
-const sidebar = document.querySelector(".sidebar");
-const sidebarText = document.getElementsByClassName("sidebar__center__list__item__text");
-const sidebarTopEl = document.querySelector(".sidebar__top__paragraph");
-const sidebarTopElText = document.querySelector(".sidebar__top__paragraph--small");
-const sidebarTopImg = document.querySelector(".sidebar__top__faceimg");
+// const dark = document.querySelector(".colorbox--dark");
+// const night = document.querySelector(".colorbox--night");
+// const light = document.querySelector(".colorbox--light");
+// const sidebarText = document.getElementsByClassName("sidebar__center__list__item__text");
+// const sidebarTopEl = document.querySelector(".sidebar__top__paragraph");
+// const sidebarTopElText = document.querySelector(".sidebar__top__paragraph--small");
+// const sidebarTopImg = document.querySelector(".sidebar__top__faceimg");
+// const sidebar = document.getElementById("sidebarid");
+// const menuMobile = document.querySelector(".menu_mobile");
+// menuMobile.addEventListener("click", () => {
+//   console.log(sidebar.className == "sidebar");
 
-//const hide = document.querySelector(".colorbox--light");
-dark.addEventListener("click", () => {
-  sidebar.className = "sidebar colorbox--dark";
-});
+//   switch (sidebar.className == "sidebar") {
+//     case true:
+//       sidebarTopElText.className = "hidden";
+//       sidebarTopImg.className = "sidebar__top__faceimg--small";
+//       sidebarTopEl.className = "hidden";
+//       sidebar.className = "sidebar--minimized";
+//       for (let i = 0; i < sidebarText.length; i + 1) {
+//         sidebarText[i].className = "hidden";
+//       }
+//       break;
+//     case false:
+//       sidebar.classList.add("sidebar");
+//       sidebar.classList.remove("sidebars");
+//       console.log("niezle2");
+//       break;
+//   }
+//   event.preventDefault();
+// });
 
-night.addEventListener("click", () => {
-  sidebar.className = "sidebar colorbox--night";
-});
+// function hide() {
+//   sidebarTopElText.className = "hidden";
+//   sidebarTopImg.className = "sidebar__top__faceimg--small";
+//   sidebarTopEl.className = "hidden";
+//   sidebar.className = "sidebar--minimized";
+//   for (let i = 0; i < sidebarText.length; i + 1) {
+//     sidebarText[i].className = "hidden";
+//   }
+// }
 
-light.addEventListener("click", () => {
-  sidebar.className = "sidebar colorbox--light";
-});
+const sidebar = document.getElementById("sidebarId");
+const menuMobile = document.querySelector(".menu_mobile");
+console.log(sidebar);
+console.log(menuMobile);
 
-light.addEventListener("click", () => {
-  sidebar.className = "sidebar--minimized";
-  hide();
-});
-
-function hide() {
-  console.log(sidebarText);
-  sidebarTopElText.className = "hidden";
-  sidebarTopImg.className = "hidden";
-  sidebarTopEl.className = "hidden";
-  for (let i = 0; i < sidebarText.length; i + 1) {
-    sidebarText[i].className = "hidden";
+menuMobile.addEventListener("click", () => {
+  switch (sidebar.className == "sidebar") {
+    case true:
+      sidebar.className = "sidebar--inactive ";
+      break;
+    case false:
+      sidebar.className = "sidebar";
+      break;
   }
-}
+  event.preventDefault();
+});
--- js/preloader.js
+++ js/preloader.js
@@ -2,6 +2,5 @@ window.onload = load;
 
 function load() {
   const preloader = document.querySelector("#loader");
-  console.log(preloader);
   preloader.className = "loader--inactive";
 }
