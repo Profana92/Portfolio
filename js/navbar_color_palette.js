cf78bc8b14e78567522a8c381f42df50566a6384
Profana92 <87131514+Profana92@users.noreply.github.com>
2022-01-18 16:15:28 +0100

    update

--- js/navbar_color_palette.js
+++ js/navbar_color_palette.js
@@ -55,3 +55,11 @@ menuMobile.addEventListener("click", () => {
   }
   event.preventDefault();
 });
+
+function openNav() {
+  document.getElementById("navigation").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navigation").style.width = "0%";
+}
