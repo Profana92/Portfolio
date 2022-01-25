babf2526ccc5fa02585e3a63e292099715cdac7e
Profana92 <87131514+Profana92@users.noreply.github.com>
2022-01-25 21:46:22 +0100

    navbar updated

--- js/navbar_color_palette.js
+++ js/navbar_color_palette.js
@@ -10,7 +10,7 @@ const sidebarCenter = document.querySelector(".sidebar__center");
 const overlay_content_arr = document.getElementsByClassName("overlay-content");
 const mediaQuery = window.matchMedia("(min-width: 768px)");
 const navLink = document.querySelectorAll("a.nav__link");
-
+const navButton = document.querySelector(".nav_open");
 // event listeners
 blue.addEventListener("click", () => {
   navi.style.backgroundColor = "var(--color-blue)";
@@ -41,36 +41,42 @@ light.addEventListener("click", () => {
   light.style.border = "1px solid #000";
 });
 
-// Functions - Timeout
+// Functions - Timeout to set navi open on load.
 
 setTimeout(function () {
   if (mediaQuery.matches) {
     document.body.style.marginLeft = "250px";
-    document.getElementById("navigation").style.width = "250px";
+    navi.style.width = "250px";
+    navButton.style.display = "none";
   }
 }, 450);
 
 // Functions - General
 
+// Open menu function
 function openNav() {
   if (mediaQuery.matches) {
     setTimeout(function () {
       document.body.style.marginLeft = "250px";
     }, 450);
-    document.getElementById("navigation").style.width = "250px";
+    navi.style.width = "250px";
   } else {
     setTimeout(function () {
       document.body.style.overflow = "hidden";
     }, 500);
-    document.getElementById("navigation").style.width = "100%";
+    navi.style.width = "100%";
   }
+  navButton.style.display = "none";
 }
-
+// Close menu function
 function closeNav() {
-  document.getElementById("navigation").style.width = "0%";
+  navi.style.width = "0%";
   document.body.style.marginLeft = "0px";
   document.body.style.overflow = "visible";
+  navButton.style.display = "inline";
 }
+
+// Resetting colorboxes border style
 function borderReset() {
   dark.style.border = "1px solid #9ca3af";
   blue.style.border = "1px solid #9ca3af";
@@ -79,6 +85,7 @@ function borderReset() {
   light.style.border = "1px solid #9ca3af";
 }
 
+// Text, borders and icons to bright color
 function toWhite() {
   borderReset();
   overlay_content_arr[0].classList.remove("content--text-dark");
@@ -89,7 +96,7 @@ function toWhite() {
     navLink[index].style.color = "white";
   }
 }
-
+// Text, borders and icons to dark color
 function toDark() {
   borderReset();
   overlay_content_arr[0].classList.add("content--text-dark");
