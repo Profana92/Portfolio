46ddd3979986ca39008e0eafa26ee3028fb04853
“Profana92” <stevevtek@gmail.com>
2023-08-27 16:22:29 +0200

    feat: 🎸 ScrollToHash Router added

---
+++ client/src/routes/pages/MyWork.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async'
+
+const MyWork = () => {
+  return (
+    <div>
+      <Helmet>
+        <title> Home | Wojciech Marczak | Let&apos;s Build The Web Together! </title>
+        <meta
+          name="description"
+          content="Welcome to my front-end developer portfolio! Take a tour of my work, skills, and projects. Let's create stunning websites together! Enjoy your visit!"
+        />
+      </Helmet>
+      home
+    </div>
+  )
+}
+
+export default MyWork
